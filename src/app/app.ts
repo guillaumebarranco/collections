018ee4d82741fb0ccd19342dddbb5110052d7218
guillaumebarranco <guillaume.barranco8@hotmail.fr>
2025-11-21 13:48:05 +0100

    change webarranco names

--- src/app/app.ts
+++ src/app/app.ts
@@ -6,8 +6,8 @@ import { RouterOutlet } from '@angular/router';
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrl: './app.scss',
 })
 export class App {
-  protected title = 'webarranco';
+  protected title = 'collections';
 }
