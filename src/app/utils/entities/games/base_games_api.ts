46426a84d8b400a154847b55736c645f1df9052f
root <root@vps-763eef2a.vps.ovh.net>
2026-01-18 14:47:45 +0000

    update ronan games from website

--- src/app/utils/entities/games/base_games_api.ts
+++ src/app/utils/entities/games/base_games_api.ts
@@ -1,3 +1,15 @@
 import { BaseGame } from '../../../models/game-model';
 
-export const baseGamesApi: BaseGame[] = [];
+export const baseGamesApi: BaseGame[] = [
+  {
+    title: 'Tetris',
+    editor: 'Nintendo',
+    hero: '',
+    coverUrl: 'https://assets-prd.ignimgs.com/2022/01/28/tetris-gb-button-edit-1643393575715.jpg',
+    releaseDate: '1984-06-06',
+    averageTimeToFinish: 9997,
+    platform: '',
+    saga: '',
+    platineTime: 0,
+  },
+];
