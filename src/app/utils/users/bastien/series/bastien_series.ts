96bfa2ff1df3a0af53849e68d9e1ae3716327ea2
root <root@vps-763eef2a.vps.ovh.net>
2026-01-19 20:44:59 +0000

    add bastien

---
+++ src/app/utils/users/bastien/series/bastien_series.ts
@@ -0,0 +1,283 @@
+import { UserSerie } from '../../../../models/serie-model';
+
+export const bastienSeries: UserSerie[] = [
+  {
+    title: 'Avatar: The Last Airbender',
+    director: 'Michael Dante DiMartino, Bryan Konietzko',
+    rating: 3.5,
+    timesWatched: 1,
+    stoppedAtSeason: 0,
+  },
+
+  {
+    title: 'Arcane',
+    director: 'Pascal Charrue, Arnaud Delord',
+    rating: 4,
+    timesWatched: 1,
+    stoppedAtSeason: 0,
+  },
+
+  {
+    title: 'Bloqués',
+    director: 'Jonathan Cohen, Ramzy Bedia',
+    rating: 4.5,
+    timesWatched: 3,
+    stoppedAtSeason: 0,
+  },
+
+  {
+    title: 'Altered Carbon',
+    director: 'Laeta Kalogridis',
+    rating: 3,
+    timesWatched: 1,
+    stoppedAtSeason: 1,
+  },
+
+  {
+    title: 'Brooklyn Nine-Nine',
+    director: 'Dan Goor, Michael Schur',
+    rating: 4,
+    timesWatched: 1,
+    stoppedAtSeason: 0,
+  },
+
+  {
+    title: 'Cloak & Dagger',
+    director: 'Joe Pokaski',
+    rating: 3,
+    timesWatched: 1,
+    stoppedAtSeason: 0,
+  },
+
+  {
+    title: 'Community',
+    director: 'Dan Harmon',
+    rating: 3.5,
+    timesWatched: 1,
+    stoppedAtSeason: 0,
+  },
+
+  {
+    title: 'Daredevil',
+    director: 'Drew Goddard',
+    rating: 4,
+    timesWatched: 1,
+    stoppedAtSeason: 0,
+  },
+
+  {
+    title: 'Hawkeye',
+    director: 'Jonathan Igla',
+    rating: 3,
+    timesWatched: 1,
+    stoppedAtSeason: 0,
+  },
+
+  {
+    title: 'Gotham',
+    director: 'Bruno Heller',
+    rating: 3,
+    timesWatched: 1,
+    stoppedAtSeason: 3,
+  },
+
+  {
+    title: 'Heroes',
+    director: 'Tim Kring',
+    rating: 3,
+    timesWatched: 1,
+    stoppedAtSeason: 3,
+  },
+
+  {
+    title: 'Iron Fist',
+    director: 'Scott Buck',
+    rating: 3,
+    timesWatched: 1,
+    stoppedAtSeason: 0,
+  },
+
+  {
+    title: 'Jessica Jones',
+    director: 'Melissa Rosenberg',
+    rating: 4,
+    timesWatched: 1,
+    stoppedAtSeason: 0,
+  },
+
+  {
+    title: 'Jupiter\'s Legacy',
+    director: 'Steven S. DeKnight',
+    rating: 4,
+    timesWatched: 1,
+    stoppedAtSeason: 0,
+  },
+
+  {
+    title: 'Locke & Key',
+    director: 'Carlton Cuse',
+    rating: 3,
+    timesWatched: 1,
+    stoppedAtSeason: 0,
+  },
+
+  {
+    title: 'Loki',
+    director: 'Kate Herron',
+    rating: 3,
+    timesWatched: 1,
+    stoppedAtSeason: 1,
+  },
+
+  {
+    title: 'Luke Cage',
+    director: 'Cheo Hodari Coker',
+    rating: 3,
+    timesWatched: 1,
+    stoppedAtSeason: 0,
+  },
+
+  {
+    title: 'Malcolm in the Middle',
+    director: 'Linwood Boomer',
+    rating: 5,
+    timesWatched: 3,
+    stoppedAtSeason: 0,
+  },
+
+  {
+    title: 'Misfits',
+    director: 'Howard Overman',
+    rating: 2.5,
+    timesWatched: 1,
+    stoppedAtSeason: 0,
+  },
+
+  {
+    title: 'Mercredi',
+    director: 'Tim Burton',
+    rating: 4,
+    timesWatched: 1,
+    stoppedAtSeason: 0,
+  },
+
+  {
+    title: 'Obi-Wan Kenobi',
+    director: 'Deborah Chow',
+    rating: 4,
+    timesWatched: 1,
+    stoppedAtSeason: 0,
+  },
+
+  {
+    title: 'One Piece (live-action)',
+    director: 'Matt Owens, Steven Maeda',
+    rating: 4,
+    timesWatched: 1,
+    stoppedAtSeason: 0,
+  },
+
+  {
+    title: 'Smallville',
+    director: 'Alfred Gough, Miles Millar',
+    rating: 3,
+    timesWatched: 1,
+    stoppedAtSeason: 0,
+  },
+
+  {
+    title: 'South Park',
+    director: 'Trey Parker, Matt Stone',
+    rating: 0,
+    timesWatched: 1,
+    stoppedAtSeason: 0,
+  },
+
+  {
+    title: 'Squid Game',
+    director: 'Hwang Dong-hyuk',
+    rating: 0,
+    timesWatched: 1,
+    stoppedAtSeason: 0,
+  },
+
+  {
+    title: 'Stranger Things',
+    director: 'The Duffer Brothers',
+    rating: 0,
+    timesWatched: 1,
+    stoppedAtSeason: 0,
+  },
+
+  {
+    title: 'Supernatural',
+    director: 'Eric Kripke',
+    rating: 0,
+    timesWatched: 1,
+    stoppedAtSeason: 0,
+  },
+
+  {
+    title: 'The Boys',
+    director: 'Eric Kripke',
+    rating: 0,
+    timesWatched: 1,
+    stoppedAtSeason: 0,
+  },
+
+  {
+    title: 'The Defenders',
+    director: 'Douglas Petrie, Marco Ramirez',
+    rating: 0,
+    timesWatched: 1,
+    stoppedAtSeason: 0,
+  },
+
+  {
+    title: 'The Falcon and the Winter Soldier',
+    director: 'Malcolm Spellman',
+    rating: 0,
+    timesWatched: 1,
+    stoppedAtSeason: 0,
+  },
+
+  {
+    title: 'The Office',
+    director: 'Greg Daniels',
+    rating: 0,
+    timesWatched: 1,
+    stoppedAtSeason: 0,
+  },
+
+  {
+    title: 'The Shannara Chronicles',
+    director: 'Alfred Gough, Miles Millar',
+    rating: 0,
+    timesWatched: 1,
+    stoppedAtSeason: 0,
+  },
+
+  {
+    title: 'The Punisher',
+    director: 'Steve Lightfoot',
+    rating: 0,
+    timesWatched: 1,
+    stoppedAtSeason: 0,
+  },
+
+  {
+    title: 'WandaVision',
+    director: 'Matt Shakman',
+    rating: 0,
+    timesWatched: 1,
+    stoppedAtSeason: 0,
+  },
+
+  {
+    title: 'Yu Yu Hakusho (live-action)',
+    director: 'Shô Tsukikawa',
+    rating: 0,
+    timesWatched: 1,
+    stoppedAtSeason: 0,
+  },
+];
