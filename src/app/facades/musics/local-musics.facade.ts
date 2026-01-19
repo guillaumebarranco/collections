79a558aa71373b75d42ef24ca7f37b28e4e2fe7e
Guillaume Barranco <guillaume.barranco8@hotmail.fr>
2026-01-19 21:22:25 +0100

    add ronan musics

--- src/app/facades/musics/local-musics.facade.ts
+++ src/app/facades/musics/local-musics.facade.ts
@@ -1,6 +1,7 @@
 import { musics as baseMusics } from '../../utils/entities/musics';
 import { musics as guillaumeMusics } from '../../utils/users/guillaume/musics';
 import { BaseMusic, UserMusic } from '../../models/music-model';
+import { ronanMusics } from '../../utils/users/ronan/musics/ronan_musics';
 
 export const allBaseMusics: BaseMusic[] = [...baseMusics];
 
@@ -8,6 +9,8 @@ export function getLocalMusicsByUser(userId: string): UserMusic[] {
   switch (userId) {
     case 'guillaume':
       return [...guillaumeMusics];
+    case 'ronan':
+      return [...ronanMusics];
     default:
       return [];
   }
