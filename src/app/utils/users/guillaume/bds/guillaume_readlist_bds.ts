6beefe9acc9244cf62d023b1f42bea13ce2d8c21
root <root@vps-763eef2a.vps.ovh.net>
2026-01-26 14:55:35 +0000

    add bd readlist

--- src/app/utils/users/guillaume/bds/guillaume_readlist_bds.ts
+++ src/app/utils/users/guillaume/bds/guillaume_readlist_bds.ts
@@ -1,3 +1,21 @@
 import { UserBd } from '../../../../models/bd-model';
 
-export const guillaumeReadListBds: UserBd[] = [];
+export const guillaumeReadListBds: UserBd[] = [
+  {
+    title: 'Goupil ou face',
+    writer: 'Lou Lubie',
+    readDate: '',
+    rating: 0,
+    readTimes: 1,
+    owned: false,
+  },
+
+  {
+    title: 'Racines',
+    writer: 'Lou Lubie',
+    readDate: '',
+    rating: 0,
+    readTimes: 1,
+    owned: false,
+  },
+];
