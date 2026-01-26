0563a8a74b8d99c9d1a44c27a6b621dd6f5370d9
root <root@vps-763eef2a.vps.ovh.net>
2026-01-26 13:11:43 +0000

    guillaume comics readlist

--- src/app/utils/users/guillaume/comics/guillaume_readlist_comics.ts
+++ src/app/utils/users/guillaume/comics/guillaume_readlist_comics.ts
@@ -1,3 +1,57 @@
 import { UserComic } from '../../../../models/comic-model';
 
-export const guillaumeReadListComics: UserComic[] = [];
+export const guillaumeReadListComics: UserComic[] = [
+  {
+    title: 'Brainiac',
+    designer: 'DC Comics',
+    readDate: '',
+    rating: 0,
+    readTimes: 1,
+    owned: false,
+  },
+
+  {
+    title: 'Killing Joke',
+    designer: 'DC Comics',
+    readDate: '',
+    rating: 0,
+    readTimes: 1,
+    owned: false,
+  },
+
+  {
+    title: 'Kingdom Come',
+    designer: 'DC Comics',
+    readDate: '',
+    rating: 0,
+    readTimes: 1,
+    owned: false,
+  },
+
+  {
+    title: 'Absolute Batman',
+    designer: 'Nick Dragotta',
+    readDate: '',
+    rating: 0,
+    readTimes: 1,
+    owned: false,
+  },
+
+  {
+    title: 'World War Hulk',
+    designer: 'John Romita Jr.',
+    readDate: '',
+    rating: 0,
+    readTimes: 1,
+    owned: false,
+  },
+
+  {
+    title: 'Wolverine : Old Man Logan',
+    designer: 'Steve McNiven',
+    readDate: '',
+    rating: 0,
+    readTimes: 1,
+    owned: false,
+  },
+];
