79ee6ea37e14ffe66eb858ce9b1bdbbf4b698516
Guillaume Barranco <guillaume.barranco8@hotmail.fr>
2026-01-26 14:23:40 +0100

    add manwhas

--- src/app/utils/entities/manwhas/base_manwhas_api.ts
+++ src/app/utils/entities/manwhas/base_manwhas_api.ts
@@ -1,3 +1,21 @@
 import { BaseManwha } from '../../../models/manwha-model';
 
-export const baseManwhasApi: BaseManwha[] = [];
+export const baseManwhasApi: BaseManwha[] = [
+  {
+    title: 'Sweet Home',
+    author: 'Young-Chan Hwang',
+    coverUrl: '/mangas_pictures/Sweet_Home_1_ki-oon.webp',
+    genre: 'Shonen',
+    nbChapters: 140,
+    isFinished: true,
+  },
+  {
+    title: 'The God of High School',
+    author: 'Yong-Je Park',
+    coverUrl:
+      '/mangas_pictures/the_god_of_high_school_tome_1-4947685-264-432.jpg',
+    genre: 'Shonen',
+    nbChapters: 435,
+    isFinished: false,
+  },
+];
