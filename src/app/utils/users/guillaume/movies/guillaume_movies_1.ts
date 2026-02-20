5f142ea6ca4ae9053eca0425b9e811e51f411ab2
Guillaume Barranco <guillaume.barranco8@hotmail.fr>
2026-02-20 12:02:29 +0100

    update g movies

--- src/app/utils/users/guillaume/movies/guillaume_movies_1.ts
+++ src/app/utils/users/guillaume/movies/guillaume_movies_1.ts
@@ -1285,7 +1285,7 @@ export const guillaumeMoviesPage1: UserMovies = [
     lastViewedDate: '',
     seenAtCinema: false,
     owned: false,
-    wantToSeeAgain: false,
+    wantToSeeAgain: true,
     watchPriority: 1,
     ratingComment: '',
   },
