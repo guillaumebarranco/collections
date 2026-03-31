660c12a24e73d692bfe2712b1d9e29e413943789
root <root@vps-763eef2a.vps.ovh.net>
2026-03-31 15:06:55 +0000

    server updates

--- src/app/utils/users/users.ts
+++ src/app/utils/users/users.ts
@@ -1,93 +1,92 @@
 export const users = [
   {
-    username: 'guillaume',
-    passwordHash:
-      'fce21eaa89c265950426846865c9e9cc0674970fd6e9d46efe169c6d2e30f223b025f75b63e0913cf5c07c4b2990aa36f2f9cb70775f0b18ac2a18335b3381d5',
-    passwordSalt: 'a89cc10fa900800ca560e2896a618e1e',
+    username: "guillaume",
+    passwordHash: "fce21eaa89c265950426846865c9e9cc0674970fd6e9d46efe169c6d2e30f223b025f75b63e0913cf5c07c4b2990aa36f2f9cb70775f0b18ac2a18335b3381d5",
+    passwordSalt: "a89cc10fa900800ca560e2896a618e1e",
     admin: true,
   },
   {
-    username: 'ronan',
-    passwordHash:
-      'efd57de093da8e8599d8126e00781403a1bd1ae7ec964ac91d6147aba5e3aa69022c5f8011db6d01b3461cae4e3a03abae5d964769434a3803cc526f6fa83d70',
-    passwordSalt: '3c03db76d19f7304396c032c96c301bc',
+    username: "ronan",
+    passwordHash: "efd57de093da8e8599d8126e00781403a1bd1ae7ec964ac91d6147aba5e3aa69022c5f8011db6d01b3461cae4e3a03abae5d964769434a3803cc526f6fa83d70",
+    passwordSalt: "3c03db76d19f7304396c032c96c301bc",
     admin: true,
   },
   {
-    username: 'cassandre',
-    passwordHash:
-      '3a0607714f1fba01dce6f425955df75c4330742c50702dfbbc2a4576e15429354e4bb8f8454921d6d6ed86fc271c5bc36c9e2a56304ed0a6bc83ce45869ee33f',
-    passwordSalt: '5afabc7fc45f7426ff6c2834b40fff1e',
+    username: "cassandre",
+    passwordHash: "3a0607714f1fba01dce6f425955df75c4330742c50702dfbbc2a4576e15429354e4bb8f8454921d6d6ed86fc271c5bc36c9e2a56304ed0a6bc83ce45869ee33f",
+    passwordSalt: "5afabc7fc45f7426ff6c2834b40fff1e",
     admin: false,
   },
   {
-    username: 'kevin',
-    passwordHash:
-      '96260f3e88622368ee54fd4294a6c213a4391d039079e04ac9a93f5ea8c65908b0bec2ebfb1c0d89dc470d10ca772bfe1302663e9dbded58a9f30f800f13de74',
-    passwordSalt: 'dc957e7e63517f6277aaef3ebcca8c7a',
+    username: "kevin",
+    passwordHash: "96260f3e88622368ee54fd4294a6c213a4391d039079e04ac9a93f5ea8c65908b0bec2ebfb1c0d89dc470d10ca772bfe1302663e9dbded58a9f30f800f13de74",
+    passwordSalt: "dc957e7e63517f6277aaef3ebcca8c7a",
     admin: true,
   },
   {
-    username: 'amandine',
-    passwordHash:
-      'fce21eaa89c265950426846865c9e9cc0674970fd6e9d46efe169c6d2e30f223b025f75b63e0913cf5c07c4b2990aa36f2f9cb70775f0b18ac2a18335b3381d5',
-    passwordSalt: 'a89cc10fa900800ca560e2896a618e1e',
+    username: "amandine",
+    passwordHash: "fce21eaa89c265950426846865c9e9cc0674970fd6e9d46efe169c6d2e30f223b025f75b63e0913cf5c07c4b2990aa36f2f9cb70775f0b18ac2a18335b3381d5",
+    passwordSalt: "a89cc10fa900800ca560e2896a618e1e",
     admin: false,
   },
   {
-    username: 'bastien',
-    passwordHash:
-      '37550d3ad1630276bb2ad702be42e64e348bb13e88fafffef4dbb5a8694bc42370b0be6ee4fc652ec450392d2c5643526e118e72293947c5913382072b93f7c6',
-    passwordSalt: 'fb3200d572dee65822d5a3d8ef5adac7',
+    username: "bastien",
+    passwordHash: "37550d3ad1630276bb2ad702be42e64e348bb13e88fafffef4dbb5a8694bc42370b0be6ee4fc652ec450392d2c5643526e118e72293947c5913382072b93f7c6",
+    passwordSalt: "fb3200d572dee65822d5a3d8ef5adac7",
     admin: false,
   },
   {
-    username: 'xeryth',
-    passwordHash:
-      'ef01a97997c958970343a216b7048b9f5674a96e3c3e45dbfdec159c276427fa59c9234da6ef76968a0a6726fc6c3afdd2cfae463d2bbdf92e8b5950815e76fe',
-    passwordSalt: 'a33dd4183c21ab37d7d4e49f6e0073b5',
+    username: "xeryth",
+    passwordHash: "ef01a97997c958970343a216b7048b9f5674a96e3c3e45dbfdec159c276427fa59c9234da6ef76968a0a6726fc6c3afdd2cfae463d2bbdf92e8b5950815e76fe",
+    passwordSalt: "a33dd4183c21ab37d7d4e49f6e0073b5",
     admin: false,
   },
   {
-    username: 'william',
-    passwordHash:
-      '7e8ef88b672d2f04b12a35e61c65a027f38f06b9c844748bf4a8ff3533d5acf17d419dcaffd2c497a5805c584c7181394ca6df7c8c1203ede0f9e7dd83f3f640',
-    passwordSalt: '7a2c5a1e82fd705ce5bb3d9ee684f2f0',
+    username: "william",
+    passwordHash: "7e8ef88b672d2f04b12a35e61c65a027f38f06b9c844748bf4a8ff3533d5acf17d419dcaffd2c497a5805c584c7181394ca6df7c8c1203ede0f9e7dd83f3f640",
+    passwordSalt: "7a2c5a1e82fd705ce5bb3d9ee684f2f0",
     admin: false,
   },
   {
-    username: 'emmanuelle',
-    passwordHash:
-      '0180d7497eb8fb03a8120a1bb4bdd2ae2dafab681257ca7e4da3a1a5b2a0d909ebdf2be0e18ae6a68552e2a9a4e2e911dce4983a197a629c530ddddb3bde5e45',
-    passwordSalt: 'e5651a544a65858867d43d93286db5d0',
+    username: "emmanuelle",
+    passwordHash: "0180d7497eb8fb03a8120a1bb4bdd2ae2dafab681257ca7e4da3a1a5b2a0d909ebdf2be0e18ae6a68552e2a9a4e2e911dce4983a197a629c530ddddb3bde5e45",
+    passwordSalt: "e5651a544a65858867d43d93286db5d0",
     admin: false,
   },
   {
-    username: 'unho',
-    passwordHash:
-      'aed4698d5b2d17cf4ca441d7c5e337af41e39ef0eef7d968624b50c7461e45faf622eaa41f76990e31b20d545d308d88de9182e5aa345fa46d7d154b8dc76c76',
-    passwordSalt: 'ef81755cb2c80233f64cc79f92a08720',
+    username: "unho",
+    passwordHash: "aed4698d5b2d17cf4ca441d7c5e337af41e39ef0eef7d968624b50c7461e45faf622eaa41f76990e31b20d545d308d88de9182e5aa345fa46d7d154b8dc76c76",
+    passwordSalt: "ef81755cb2c80233f64cc79f92a08720",
     admin: false,
   },
   {
-    username: 'dantes',
-    passwordHash:
-      'd000d41cd2ef3d414591c1a13f30a2dca89eacc4231807551994cc41d89d0b4df7510f2b40e92a5901016a38144ffafab4625aca42b63dd65b8a612f60d3f914',
-    passwordSalt: 'b13cb41548862d8e6ade02950b0d2d5b',
+    username: "dantes",
+    passwordHash: "d000d41cd2ef3d414591c1a13f30a2dca89eacc4231807551994cc41d89d0b4df7510f2b40e92a5901016a38144ffafab4625aca42b63dd65b8a612f60d3f914",
+    passwordSalt: "b13cb41548862d8e6ade02950b0d2d5b",
     admin: false,
   },
   {
-    username: 'marina',
-    passwordHash:
-      '7227ccd04637c1c3d8fc2d11efe57f07c032fb24885c87cb0e35e674cedeed6e079fb6ad03b1694a73f30b640adcb932e6161f09e5d8cd0c7b63f2a32e48bc05',
-    passwordSalt: '1a4d7d25eb30e26b882d081abaaad466',
+    username: "marina",
+    passwordHash: "7227ccd04637c1c3d8fc2d11efe57f07c032fb24885c87cb0e35e674cedeed6e079fb6ad03b1694a73f30b640adcb932e6161f09e5d8cd0c7b63f2a32e48bc05",
+    passwordSalt: "1a4d7d25eb30e26b882d081abaaad466",
     admin: false,
   },
   {
-    username: 'masterofmadness',
-    passwordHash:
-      '9cd3dbd906f2808073e0d3c4c18b39b13a194ca03a322a11e47a1cf58c699de43c6ddfbe54a42171dc72adffc922e00c145799151b365f0a05b1bde82b5ccda8',
-    passwordSalt: 'ea7e3e460c63147979a4e0d665389145',
+    username: "masterofmadness",
+    passwordHash: "9cd3dbd906f2808073e0d3c4c18b39b13a194ca03a322a11e47a1cf58c699de43c6ddfbe54a42171dc72adffc922e00c145799151b365f0a05b1bde82b5ccda8",
+    passwordSalt: "ea7e3e460c63147979a4e0d665389145",
     admin: false,
   },
+  {
+    username: "gigi",
+    passwordHash: "19d0e171537888bf4050d0a554d991159b10080de66c88f881698a2f19750e894558ee395fd101373dc5b7ddd76d5752f58523c0be01a4b48fc003948088db91",
+    passwordSalt: "678025d8a558b7bc9d54a170cda24002",
+    admin: false,
+  },
+  {
+    username: "gigipentest",
+    passwordHash: "5f2226449e72a4b36271e6fbea757b4c883f02a176f07ff18c390b5dd83c711d296bcb9c1c30be0d0fa5e0ce4f2963f92694c311f7ed2bcc6fec4de53a03e522",
+    passwordSalt: "95e9cbcd6bf4e5220b1e83cb7404c119",
+    admin: false,
+  }
 ];
