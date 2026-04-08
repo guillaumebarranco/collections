d327c9a6ecf784d595701626defc2a6b087875ae
Guillaume Barranco <guillaume.barranco8@hotmail.fr>
2026-04-08 16:19:33 +0200

    update mix

--- src/app/utils/entities/comics/base_comics_api.ts
+++ src/app/utils/entities/comics/base_comics_api.ts
@@ -2,30 +2,26 @@ import { BaseComic } from '../../../models/comic-model';
 
 export const baseComicsApi: BaseComic[] = [
   {
-    title: "Absolute Wonder Woman T.1",
-    writer: "Kelly Thompson",
-    designer: "Hayden Sherman",
-    coverUrl: "/comics_pictures/11ece9cf68e5.jpg",
+    title: 'Absolute Wonder Woman T.1',
+    writer: 'Kelly Thompson',
+    designer: 'Hayden Sherman',
+    coverUrl: '/comics_pictures/11ece9cf68e5.jpg',
     pages: 144,
-    genre: "Super-héro",
-    saga: "Absolute Wonder Woman",
+    genre: 'Super-héro',
+    saga: 'Absolute Wonder Woman',
     sagaOrder: 1,
-    description: "",
+    description: '',
   },
   {
-    title: "Absolute Martian Manhunter T.1",
-    writer: "Deniz Camp",
-    designer: "Javier Rodriguez",
-<<<<<<< Updated upstream
-    coverUrl: "/comics_pictures/10ae5b76041f.jpg",
-    pages: 0,
-=======
-    coverUrl: "https://m.media-amazon.com/images/I/81U5SBvhrjL._AC_UF1000,1000_QL80_.jpg",
+    title: 'Absolute Martian Manhunter T.1',
+    writer: 'Deniz Camp',
+    designer: 'Javier Rodriguez',
+    coverUrl:
+      'https://m.media-amazon.com/images/I/81U5SBvhrjL._AC_UF1000,1000_QL80_.jpg',
     pages: 152,
->>>>>>> Stashed changes
-    genre: "Super-héro",
-    saga: "Absolute Martian Manhunter",
+    genre: 'Super-héro',
+    saga: 'Absolute Martian Manhunter',
     sagaOrder: 1,
-    description: "",
-  }
+    description: '',
+  },
 ];
