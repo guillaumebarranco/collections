27127aef122309854b99ce450f0b6c5782f2ac5b
root <root@vps-763eef2a.vps.ovh.net>
2026-04-28 10:04:37 +0000

    server updates

--- src/app/utils/users/guillaume/movies/guillaume_movies_1.ts
+++ src/app/utils/users/guillaume/movies/guillaume_movies_1.ts
@@ -2846,4 +2846,21 @@ export const guillaumeMoviesPage1: UserMovies = [
     borrowed: '',
     loaned: '',
   },
+
+  {
+    title: "Là-haut",
+    director: "Pete Docter",
+    rating: 4.25,
+    timesWatched: 1,
+    firstViewedDate: "2026-04-27",
+    lastViewedDate: "2026-04-27",
+    seenAtCinema: false,
+    owned: false,
+    wantToSeeAgain: false,
+    watchPriority: 1,
+    ratingComment: "",
+    inList: [],
+    borrowed: "",
+    loaned: "",
+  },
 ];
