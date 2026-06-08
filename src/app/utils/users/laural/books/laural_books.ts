fa5cb51ce7069a2e35aac1d7d95e440b3c3d9e8f
Guillaume Barranco <guillaume.barranco8@hotmail.fr>
2026-06-08 09:25:59 +0200

    add l movies

--- src/app/utils/users/laural/books/laural_books.ts
+++ src/app/utils/users/laural/books/laural_books.ts
@@ -2,19 +2,19 @@ import { UserBook } from '../../../../models/book-model';
 
 export const lauralBooks: UserBook[] = [
   {
-    title: "La Maison au milieu de la Mer Céruléenne",
-    author: "T.J. Klune",
-    firstReadDate: "2025-02-18",
-    lastReadDate: "",
+    title: 'La Maison au milieu de la Mer Céruléenne',
+    author: 'T.J. Klune',
+    firstReadDate: '2025-02-18',
+    lastReadDate: '',
     otherReadDates: [],
     rating: 4,
     reading: false,
     readTimes: 1,
     owned: false,
-    borrowed: "",
-    loaned: "",
-    readPriority: 0,
+    borrowed: '',
+    loaned: '',
+    readPriority: 1,
     wantToReadAgain: false,
-    ratingComment: "",
-  }
+    ratingComment: '',
+  },
 ];
